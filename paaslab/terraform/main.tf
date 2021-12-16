4d171747079c5ab2d717e2d77fdae43e66f76d87
Larry Claman <25161683+larryclaman@users.noreply.github.com>
2021-12-16 10:20:56 -0500

    change

--- paaslab/terraform/main.tf
+++ paaslab/terraform/main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.89.0"
+      version = "2.88.1"
     }
     random = {
       source  = "hashicorp/random"
